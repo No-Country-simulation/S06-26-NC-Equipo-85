04f22851948193c6dbef0c784e88cfe0befaaa9a
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2026-06-11 16:58:44 -0500

    feat: add accessibility and store hydration components, update imports for UI components

--- packages/ui/src/atoms/dialog/dialog.tsx
+++ packages/ui/src/atoms/dialog/dialog.tsx
@@ -3,15 +3,9 @@
 import * as React from "react";
 import { Dialog as DialogPrimitive } from "radix-ui";
 
-<<<<<<<< HEAD:packages/ui/src/atoms/dialog/dialog.tsx
-import { cn } from "../../lib/utils";
+import { cn } from "@app/ui/lib/utils";
 import { Button } from "../button";
 import { XIcon } from "lucide-react";
-========
-import { cn } from "@appbit/ui/lib/utils"
-import { Button } from "@appbit/ui/components/button"
-import { XIcon } from "lucide-react"
->>>>>>>> 51bb92f35893b8eb5db168637904bcafd59ee8fa:packages/ui/src/components/ui/dialog.tsx
 
 function Dialog({
   ...props
--- packages/ui/src/atoms/emoji-check-in/emoji-check-in.tsx
+++ packages/ui/src/atoms/emoji-check-in/emoji-check-in.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { motion } from "framer-motion";
 
-import { cn } from "../../lib/utils";
+import { cn } from "@app/ui/lib/utils";
 
 export type Mood = "feliz" | "cansado" | "triste" | "ansioso" | "sobrecargado";
 
